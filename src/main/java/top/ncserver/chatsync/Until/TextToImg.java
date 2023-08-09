e6f77b5a2f4d5f611301c97c3038766a50595331
1zyao <107829654+1zyao@users.noreply.github.com>
2023-08-09 23:40:47 +0800

    修改渲染方式
    我试了试我电脑 tps渲染在180ms左右 help在1000左右 原版tps渲染200左右 help1200左右

--- src/main/java/top/ncserver/chatsync/Until/TextToImg.java
+++ src/main/java/top/ncserver/chatsync/Until/TextToImg.java
@@ -1,130 +1,147 @@
 package top.ncserver.chatsync.Until;
 
-
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TextToImg {
-    public static File toImg(String text) throws IOException {
-        String[] strings =text.split("\n");
-        int minX=0;
+    private static final Map<Character, Color> colorMap = new HashMap<>();
 
-        Font font =null;
+    static {
+        colorMap.put('0', Color.black);
+        colorMap.put('1', new Color(Integer.parseInt("0000AA", 16)));
+        colorMap.put('2', new Color(Integer.parseInt("00AA00", 16)));
+        colorMap.put('3', new Color(Integer.parseInt("00AAAA", 16)));
+        colorMap.put('4', new Color(Integer.parseInt("AA0000", 16)));
+        colorMap.put('5', new Color(Integer.parseInt("AA00AA", 16)));
+        colorMap.put('6', new Color(Integer.parseInt("FFAA00", 16)));
+        colorMap.put('7', new Color(Integer.parseInt("AAAAAA", 16)));
+        colorMap.put('8', new Color(Integer.parseInt("555555", 16)));
+        colorMap.put('9', new Color(Integer.parseInt("5555FF", 16)));
+        colorMap.put('a', new Color(Integer.parseInt("55FF55", 16)));
+        colorMap.put('b', new Color(Integer.parseInt("55FFFF", 16)));
+        colorMap.put('c', new Color(Integer.parseInt("FF5555", 16)));
+        colorMap.put('d', new Color(Integer.parseInt("FF55FF", 16)));
+        colorMap.put('e', new Color(Integer.parseInt("FFFF55", 16)));
+        colorMap.put('f', new Color(Integer.parseInt("FFFFFF", 16)));
+        colorMap.put('g', new Color(Integer.parseInt("DDD605", 16)));
+    }
+
+    public static File toImg(String text) throws IOException {
+        String[] strings = text.split("\n");
+        int minX = 0;
+
+        Font font = null;
         try {
-            font= Font.createFont(Font.TRUETYPE_FONT, TextToImg.class.getResourceAsStream("/MiSans-Normal.ttf"));
-            font=font.deriveFont(64F);
+            font = Font.createFont(Font.TRUETYPE_FONT, TextToImg.class.getResourceAsStream("/MiSans-Normal.ttf"));
+            font = font.deriveFont(64F);
         } catch (FontFormatException e) {
             throw new RuntimeException(e);
         }
-        for (String line: strings) {
 
-            line=line.replaceAll("ﾧ\\S", "");
-            line=line.replaceAll("§\\S", "");
-            //font.getLineMetrics(line)
-            FontMetrics fm = Toolkit.getDefaultToolkit().getFontMetrics(font);
+        FontMetrics fm = Toolkit.getDefaultToolkit().getFontMetrics(font);
+        for (String line : strings) {
+            line = line.replaceAll("ﾧ\\S", "");
+            line = line.replaceAll("§\\S", "");
             int result = fm.stringWidth(line);
 
-            if (minX<result) minX=result;
+            if (minX < result) minX = result;
         }
-        int Y= strings.length*68+ (strings.length-1)*16+30;
-        minX=minX+64;
 
+        int lineHeight = 68;
+        int lineSpacing = 16;
+        int Y = strings.length * lineHeight + (strings.length - 1) * lineSpacing + 30;
+        minX = minX + 64;
 
-        BufferedImage image = new BufferedImage(minX, Y,
-                BufferedImage.TYPE_INT_BGR);
-        Graphics g = image.getGraphics();
-        g.setClip(0, 0, minX, Y);
+        BufferedImage image = new BufferedImage(minX, Y, BufferedImage.TYPE_INT_BGR);
+        Graphics2D g = image.createGraphics();
         g.setColor(new Color(Integer.parseInt("cacdca", 16)));
-        g.fillRect(0, 0,minX, Y);// 先用黑色填充整张图片,也就是背景
-        g.setColor(Color.black);// 在换成黑色
-        g.setFont(font);// 设置画笔字体
-        for (int i=0;i< strings.length;i++){
-            String nowLine=strings[i];
-            int dex=0;
-            int nowX=32;
+        g.fillRect(0, 0, minX, Y);
+        g.setColor(Color.black);
+        g.setFont(font);
 
-            for (int j = 0; j < nowLine.length(); j++) {
-                if (nowLine.charAt(j)=='ﾧ'||nowLine.charAt(j)=='§'){
-                    switch (nowLine.charAt(j+1)){
-                        case '0':
-                            g.setColor(Color.black);
-                            break;//黑色
-                        case '1':
-                            g.setColor(new Color(Integer.parseInt("0000AA", 16)));
-                            break;//深蓝色
-                        case '2':
-                            g.setColor(new Color(Integer.parseInt("00AA00", 16)));
-                            break;//深绿色
-                        case '3':
-                            g.setColor(new Color(Integer.parseInt("00AAAA", 16)));
-                            break;//湖蓝色
-                        case '4':
-                            g.setColor(new Color(Integer.parseInt("AA0000", 16)));
-                            break;//深红色
-                        case '5':
-                            g.setColor(new Color(Integer.parseInt("AA00AA", 16)));
-                            break;//紫色
-                        case '6':
-                            g.setColor(new Color(Integer.parseInt("FFAA00", 16)));
-                            break;//金色
-                        case '7':
-                            g.setColor(new Color(Integer.parseInt("AAAAAA", 16)));
-                            break;//灰色
-                        case '8':
-                            g.setColor(new Color(Integer.parseInt("555555", 16)));
-                            break;//深灰色
-                        case '9':
-                            g.setColor(new Color(Integer.parseInt("5555FF", 16)));
-                            break;//蓝色
-                        case 'a':
-                            g.setColor(new Color(Integer.parseInt("55FF55", 16)));
-                            break;//绿色
-                        case 'b':
-                            g.setColor(new Color(Integer.parseInt("55FFFF", 16)));
-                            break;//天蓝色
-                        case 'c':
-                            g.setColor(new Color(Integer.parseInt("FF5555", 16)));
-                            break;//红色
-                        case 'd':
-                            g.setColor(new Color(Integer.parseInt("FF55FF", 16)));
-                            break;//粉红色
-                        case 'e':
-                            g.setColor(new Color(Integer.parseInt("FFFF55", 16)));
-                            break;//黄色
-                        case 'f':
-                            g.setColor(new Color(Integer.parseInt("FFFFFF", 16)));
-                            break;//白色
-                        case 'g':
-                            g.setColor(new Color(Integer.parseInt("DDD605", 16)));
-                            break;//硬币金
-                        default:
-                            g.setColor(Color.WHITE);
+        for (int i = 0; i < strings.length; i++) {
+            renderLineAndColor(strings[i], i, g, lineHeight, lineSpacing);
+        }
+
+        g.dispose();
+        File outfile = new File("image.png");
+        ImageIO.write(image, "png", outfile);
+        return outfile;
+    }
+
+    private static synchronized void renderLineAndColor(String line, int lineIndex, Graphics g, int lineHeight, int lineSpacing) {
+        int dex = 0;
+        int nowX = 32;
+        FontMetrics fm = g.getFontMetrics();
+
+        for (int j = 0; j < line.length(); j++) {
+            char currentChar = line.charAt(j);
+            if (currentChar == 'ﾧ' || currentChar == '§') {
+                Color color = getColor(line.charAt(j + 1));
+                if (color != null) {
+                    g.setColor(color);
+                } else {
+                    g.setColor(Color.WHITE);
+                }
+                j++;
+                dex = dex + 2;
+            } else {
+                String b = Character.toString(currentChar);
+                g.drawString(b, nowX, lineIndex > 0 ? lineHeight + (lineIndex) * (lineHeight + lineSpacing) : lineHeight);
+                nowX += fm.stringWidth(b);
+                dex++;
+            }
+        }
+    }
+
+    private static Color getColor(char colorCode) {
+        return colorMap.get(colorCode);
+    }
+
+    private static void renderLine(String line, int lineIndex, Graphics2D g, int lineHeight, int lineSpacing) {
+        int nowX = 32;
+        FontMetrics fm = g.getFontMetrics();
+        int y = lineIndex > 0 ? lineHeight + (lineIndex) * (lineHeight + lineSpacing) : lineHeight;
+
+        if (line.length() < 10) {
+            int dex = 0;
+
+            for (int j = 0; j < line.length(); j++) {
+                if (line.charAt(j) == 'ﾧ' || line.charAt(j) == '§') {
+                    Color color = colorMap.get(line.charAt(j + 1));
+                    if (color != null) {
+                        g.setColor(color);
+                    } else {
+                        g.setColor(Color.WHITE);
                     }
                     j++;
-                    dex=dex+2;
-
-                }else {
-                    String b=Character.toString(nowLine.charAt(j));
-                    g.drawString(String.valueOf(nowLine.charAt(dex)), nowX, i>0?68+(i)*68+(i)*16:68);
-                    FontMetrics fm = Toolkit.getDefaultToolkit().getFontMetrics(font);
-                    nowX+= fm.charWidth(nowLine.charAt(dex));
+                    dex = dex + 2;
+                } else {
+                    String b = Character.toString(line.charAt(dex));
+                    g.drawString(b, nowX, lineIndex > 0 ? lineHeight + (lineIndex) * (lineHeight + lineSpacing) : lineHeight);
+                    nowX += fm.stringWidth(b);
                     dex++;
                 }
             }
-            System.out.println(strings[i]);
+        } else {
+            BufferedImage textImage = new BufferedImage(fm.stringWidth(line), lineHeight, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D textGraphics = textImage.createGraphics();
 
+            textGraphics.setColor(Color.WHITE);
+            textGraphics.fillRect(0, 0, textImage.getWidth(), textImage.getHeight());
+
+            textGraphics.setColor(g.getColor());
+            textGraphics.setFont(g.getFont());
+            textGraphics.drawString(line, 0, fm.getAscent());
+
+            textGraphics.dispose();
+
+            g.drawImage(textImage, nowX, y, null);
         }
-        g.dispose();
-        File outfile = new File("image.png");
-
-        ImageIO.write(image, "png", outfile);
-
-        return outfile;
     }
 }
